91c39ba7c64542b2b28fdc9dd3db05e7354a5dba
田中　龍 <tanaka@anagram-works.jp>
2024-06-07 14:03:26 +0900

    現状分

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,12 +1,18 @@
 import { getPostBySlug, getAllSlugs } from "lib/api";
 import { extractText } from "lib/extract-text";
+import { prevNextPost } from "lib/prev-next-post";
 import Container from "components/container";
 import PostHeader from "components/post-header";
 import PostBody from "components/post-body";
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
 import ConvertBody from "components/convert-body";
 import PostCategories from "components/post-categories";
+import Pagination from "components/pagination";
 import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants";
 
 // サイトに関する情報
 import { siteMeta } from "lib/constants"
@@ -30,7 +36,7 @@ export default async function Post({ params }) {
   // eyecatch.blurDataURL = base64
 
   const allSlugs = await getAllSlugs()
-  // const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
 
   return (
     <Container>
@@ -62,6 +68,12 @@ export default async function Post({ params }) {
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   )
