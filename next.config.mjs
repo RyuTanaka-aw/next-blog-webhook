c1c9025a7e9f5ff6410c4e6375add74e8f9f50f9
田中　龍 <tanaka@anagram-works.jp>
2024-07-09 09:56:49 +0900

    ルートディレクトリを変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // distDir: 'next-blog-ssg',
   trailingSlash: true,
-  assetPrefix: '/react/next-blog-ssg',
-  basePath: '/react/next-blog-ssg',
   images: {
     loader: 'custom',
     path: '/lib/image.js'
